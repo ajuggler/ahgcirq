21a106cb277580153241f540bcf69994eb188136
ajuggler <ajuggler@users.noreply.github.com>
2020-11-02 23:51:15 -0600

    Create a bell state

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ahgcirq
- Programas Cirq para el curso de CÃ³mputo CuÃ¡ntico
+ Programas Cirq para el curso de Cómputo Cuántico, impartido por el profesor Antonio Hernández Garduño en el ITAM (México).
+ 
+ Estos programas son ligeras modificaciones de las versiones que se pueden bajar de 
+ 
+https://github.com/JackHidary/quantumcomputingbook
---
+++ simple.py
@@ -0,0 +1,29 @@
+"""Simple program in Cirq."""
+
+# Import the Cirq package
+import cirq
+
+# Pick a qubit
+qubit = cirq.GridQubit(0, 0)
+
+# Create a circuit
+circuit = cirq.Circuit([
+    cirq.X(qubit),  # NOT  #  cirq.H(cirq.X(qubit))
+    cirq.H(qubit),
+    cirq.measure(qubit, key='m')  # Measurement
+    ]
+)
+
+# Display the circuit
+print("Circuit:")
+print(circuit)
+
+# Get a simulator to execute the circuit
+simulator = cirq.Simulator()
+
+# Simulate the circuit several times
+result = simulator.run(circuit, repetitions=10)
+
+# Print the results
+print("Results:")
+print(result)
